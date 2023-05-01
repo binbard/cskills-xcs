4cbbcbbe2b45bec934ddb43ca7b30ca2f604a31b
DAVINCI\notok <harshitjawla123@gmail.com>
2023-05-01 13:43:18 +0530

    Done 3

--- server.js
+++ server.js
@@ -8,26 +8,33 @@ let qna = [];
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+  console.log('a user connected');
 
-    socket.emit('init', qna);
+  socket.emit('init', qna);
 
-    socket.on('qna', (data) => {
-        console.log(`question: ${data.question} | answer: ${data.answer} | name: ${data.name}`);
-        if (data.name === '!1') {
-            qna = [];
-            io.emit('clear qna');
-        } else {
-            qna.push({ question: data.question, answer: data.answer, name: data.name });
-            io.emit('new qna', { question: data.question, answer: data.answer, name: data.name });
-        }
-    });
+  socket.on('qna', (data) => {
+    console.log(`question: ${data.question} | answer: ${data.answer} | name: ${data.name}`);
+    if (data.name === '!1') {
+      qna = [];
+      io.emit('clear qna');
+    }
+    else if (data.name === '!2') {
+      io.emit('clear all');
+    }
+    else if (data.name === '!3') {
+      qna = [];
+      io.emit('clear del all');
+    } else {
+      qna.push({ question: data.question, answer: data.answer, name: data.name });
+      io.emit('new qna', { question: data.question, answer: data.answer, name: data.name });
+    }
+  });
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 http.listen(80, () => {
-    console.log('listening on *:80');
+  console.log('listening on *:80');
 });
